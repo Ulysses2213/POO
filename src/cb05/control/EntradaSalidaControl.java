ac98bbbb9fa4b5b3cf9af72e046db672d47b5c2c
Ulysses2213 <56575335+Ulysses2213@users.noreply.github.com>
2020-02-16 05:36:21 -0600

    Se modifican pantallas de entrada y salida, se implementa los controles validaciones para cada uno de los campos.

--- src/cb05/control/EntradaSalidaControl.java
+++ src/cb05/control/EntradaSalidaControl.java
@@ -4,22 +4,28 @@
  * and open the template in the editor.
  */
 package cb05.control;
+import cb05.EntradaSalida;
 import javax.swing.JOptionPane;
 /**
  *
  * @author gem2u
  */
 public class EntradaSalidaControl {
+    
+    EntradaSalida vistaES;
      
     public void validacionCampos(int b1, int b2, String numero){
         if(b1 == b2 && b1 != 0 && b2 !=0){
             JOptionPane.showMessageDialog(null, "Las bases son iguales, debe elegir otras.");
+            limpiarDatos();
         }else{
             if(b1 == 0 || b2 == 0){
                 JOptionPane.showMessageDialog(null, "Debe elegir alguna base.");
+                limpiarDatos();
             }else{
                 if(numero.isEmpty() && (b1 != 0 || b2 != 0)){
                     JOptionPane.showMessageDialog(null, "Debe llenar el campo del numero.");
+                    limpiarDatos();
                 }
             }     
         }
@@ -27,9 +33,22 @@ public class EntradaSalidaControl {
     
     public void validacionNumero(char validacion, int b1, java.awt.event.KeyEvent evt){
         char validar = evt.getKeyChar();
+        b1 -=1;
+        char num = Integer.toString(b1).charAt(0);
         if(Character.isDigit(validar)){
-            
+            if(validar > num){
+                JOptionPane.showMessageDialog(null, "Debe ingresar numeros validos");
+                limpiarDatos();
+            }
         }
         
+    }
+    
+    public void limpiarDatos(){
+        vistaES.cbB1.setSelectedIndex(0);
+        vistaES.cbB2.setSelectedIndex(1);
+        vistaES.lbResultadoDigitosPermitidos.setText("");
+        vistaES.lbResultadoFinal.setText("");
+        vistaES.tfEntradaNumero.setText("");
     }
 }
